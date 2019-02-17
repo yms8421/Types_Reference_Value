38bf249de774b311fab3a902ad2e65408716ba0b
yms8421 <46646219+yms8421@users.noreply.github.com>
2019-02-17 14:26:34 +0300

    Add files via upload

---
+++ BilgeAdam.Types/BilgeAdam.Types/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BilgeAdam.Types
+{
+    class Program
+    {
+
+        public static byte[] ReadFully(Stream input)
+        {
+            byte[] buffer = new byte[16 * 1024];
+            using (MemoryStream ms = new MemoryStream())
+            {
+                int read;
+                while ((read = input.Read(buffer, 0, buffer.Length)) > 0)
+                {
+                    ms.Write(buffer, 0, read);
+                }
+                return ms.ToArray();
+            }
+        }
+        static void Main(string[] args)
+        {
+            var i = 300;
+            var i1 = Convert.ToByte(i);
+
+            var stream = new FileStream(@"C:\Code\kalp.png", FileMode.Open);
+            var bytes = ReadFully(stream);
+
+            var l = 3534553545L;
+            var l1 = (int)l;
+            var a = "9";
+            var n = Convert.ToInt32(a);
+            var m = int.Parse(a);
+            
+
+            var c = 9D;
+            var c1 = Convert.ToInt32(c);
+            var c2 = (int)c;//boxing
+
+            var d = 9;
+            var d1 = (double)d;//unboxing
+
+            object t = "Can Perk - 08.02.1988";
+
+            Person p = (Person)t;//çeviremezse exception fırlatır
+            Person r = t as Person;//çeviremezse null değer üretir
+            Console.WriteLine(p.name);
+            var s = (Person)t;
+            var u = t as Person;
+        }
+    }
+
+    class Person
+    {
+        public string name;
+        public string birthDate;
+    }
+}
---
+++ BilgeAdam.Types/BilgeAdam.Types2/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BilgeAdam.Types2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = 8;
+            var b = a;
+            a = 7;
+
+            var c = 3.4;
+            var d = c;
+            c++;
+
+            Console.WriteLine("A: " + a);
+            Console.WriteLine("B: " + b);
+
+            Console.WriteLine("-----------------");
+            var numbers = new int[] { 2, 5, 12, 6, 7 };
+            var others = numbers;
+            numbers[2] = 0;
+            others[4] = 21;
+
+            Console.Write("numbers...: ");
+            foreach (var item in numbers)
+            {
+                Console.Write("{0} ", item);
+            }
+            Console.WriteLine();
+            Console.Write("others....: ");
+            foreach (var item in others)
+            {
+                Console.Write("{0} ", item);
+            }
+
+            var p1 = new Kisi
+            {
+                name = "Can PERK",
+                birthDate = new DateTime(1988, 2, 8),
+                height = 183,
+                city = "Kars",
+                status = MaritialStatus.Married
+            };
+
+            var p2 = p1;
+            p2.height = 173;
+            p2.name = "Deniz ASLAN";
+            p2.city = "Ankara";
+            p2.status = MaritialStatus.Single;
+            p2.birthDate = p2.birthDate.AddYears(8).AddMonths(4);
+
+            var deger = "sdfsdfsjkdsdkjfhsdjkf";
+            deger.Replace("s", "a");
+
+
+            var text = "hamdım yandım piştim";//immutable
+            text = text.ToUpper().Replace("M", "N");
+            //text = "Gittim gördüm gezdim";
+            Console.ReadKey();
+        }
+    }
+
+    struct Kisi
+    {
+        public string name;
+        public DateTime birthDate;
+        public string city;
+        public int height;
+        public MaritialStatus status;
+    }
+
+    enum MaritialStatus
+    {
+        Married = 0,
+        Single = 1
+    }
+}
+
+/*
+ 
+ Reference Type : class
+ Value Type     : struct
+     
+     */
